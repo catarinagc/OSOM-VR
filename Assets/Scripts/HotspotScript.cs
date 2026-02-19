20ab7b5c25da241a99d6c6e654076dd88f506d26
catarinagc <catarinagcostaa@gmail.com>
2026-02-19 12:22:34 +0000

    changed hotspot

--- Assets/Scripts/HotspotScript.cs
+++ Assets/Scripts/HotspotScript.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class HotspotScript : MonoBehaviour
 {
     [SerializeField] public Vector2 realWorldPosition;
     [SerializeField] GameObject hotspotImageObj;
+    [SerializeField] int hotspotID;
+
+    [SerializeField] private GameObject textID;
+
     private bool isActive = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        textID.GetComponent<TextMeshProUGUI>().text = hotspotID.ToString();
     }
 
     // Update is called once per frame
