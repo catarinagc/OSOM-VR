5e492355918a380c59cdc0c2752d8fa14dd01f65
catarinagc <catarinagcostaa@gmail.com>
2026-02-24 13:14:22 +0000

    hotspot position logic done

--- Assets/Scripts/HotspotManager.cs
+++ Assets/Scripts/HotspotManager.cs
@@ -1,28 +1,90 @@
 using UnityEngine;
-
-public class NewMonoBehaviourScript : MonoBehaviour
+using System.IO;
+using System.Globalization;
+public class HotspotManager : MonoBehaviour
 {
     [SerializeField] GameObject breakwaterOrigin;
     [SerializeField] float modelScale;
+    Vector3 realOriginPos;
+    [SerializeField] GameObject hotspotPrefab;
+    //public string fileName = "hotspots_Data.csv";
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //procurar todos os GameObjects com Tag "Hotspot"
-        Vector3 realOriginPos = breakwaterOrigin.GetComponent<originPointScript>().realWorldPosition;
-        GameObject[] hotspots = GameObject.FindGameObjectsWithTag("Hotspot");
-        for (int i = 0; i < hotspots.Length; i++)
-        {
-            Vector3 realHotspotPos = hotspots[i].GetComponent<HotspotScript>().realWorldPosition;
-            Vector3 ofsset = (realHotspotPos - realOriginPos) * modelScale;
-            hotspots[i].transform.position = breakwaterOrigin.transform.position + ofsset;
-        }
+        realOriginPos = breakwaterOrigin.GetComponent<originPointScript>().realWorldPosition;
+        ReadCSV();
 
+        // GameObject[] hotspots = GameObject.FindGameObjectsWithTag("Hotspot");
+        // for (int i = 0; i < hotspots.Length; i++)
+        // {
+        //     Vector3 realHotspotPos = hotspots[i].GetComponent<HotspotScript>().realWorldPosition;
+        //     float localX = realHotspotPos.x - realOriginPos.x;
+        //     float localY = realHotspotPos.y - realOriginPos.y;
+        //     localX *= modelScale;
+        //     localY *= modelScale;
+        //     Vector3 offset = new Vector3(localX, 0f, localY);
+        //     //hotspots[i].transform.position = breakwaterOrigin.transform.position + offset;
+        //     hotspots[i].transform.SetParent(breakwaterOrigin.transform);
+        //     hotspots[i].transform.localPosition = -offset;
+        // }
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    void ReadCSV()
+    {
+        string path = Path.Combine(Application.dataPath, "HotspotData/Hotspot_Data.csv");
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError("CSV file not found: " + path);
+            return;
+        }
+
+        string[] lines = File.ReadAllLines(path);
+
+        // Start from line index 2
+        for (int i = 2; i < lines.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(lines[i]))
+                continue;
+
+            string[] columns = lines[i].Split(',');
+
+            string id = columns[0];
+
+            float x = float.Parse(columns[1], CultureInfo.InvariantCulture);
+            float y = float.Parse(columns[2], CultureInfo.InvariantCulture);
+
+            //Debug.Log($"ID: {id} | X: {x} | Y: {y}");
+
+            GameObject hotspot = CreateHotspot(id, new Vector2(x, y));
+            ChangeHotspotPosition(hotspot);
+        }
+    }
+
+    void ChangeHotspotPosition(GameObject hotspot)
+    {
+        Vector3 realHotspotPos = hotspot.GetComponent<HotspotScript>().realWorldPosition;
+        float localX = realHotspotPos.x - realOriginPos.x;
+        float localY = realHotspotPos.y - realOriginPos.y;
+        localX *= modelScale;
+        localY *= modelScale;
+        Vector3 offset = new Vector3(localX, 0f, localY);
+        //hotspots[i].transform.position = breakwaterOrigin.transform.position + offset;
+        hotspot.transform.SetParent(breakwaterOrigin.transform);
+        hotspot.transform.localPosition = -offset;
+    }
+
+    GameObject CreateHotspot(string ID, Vector2 realPos)
+    {
+        GameObject newHotspot = Instantiate(hotspotPrefab);
+
+        newHotspot.name = "Hotspot_" + ID;
+
+        // Store real-world position in script
+        HotspotScript hs = newHotspot.GetComponent<HotspotScript>();
+        hs.hotspotID = int.Parse(ID);
+        hs.realWorldPosition = new Vector2(realPos.x, realPos.y);
+
+        return newHotspot;
+    }
 }
--- Assets/Scripts/HotspotScript.cs
+++ Assets/Scripts/HotspotScript.cs
@@ -6,7 +6,7 @@ public class HotspotScript : MonoBehaviour
 {
     [SerializeField] public Vector2 realWorldPosition;
     [SerializeField] GameObject hotspotImageObj;
-    [SerializeField] int hotspotID;
+    [SerializeField] public int hotspotID;
 
     [SerializeField] private GameObject textID;
 
